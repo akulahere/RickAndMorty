938ac7e39fe456fef551db93da8d14599ca1f607
Dmytro Akulinin <akulinind@gmail.com>
2023-02-20 12:50:47 +0200

    feat: add photo to detailed info character

--- RickAndMorty/ViewModels/CharacterDetail/RMCharacterPhotoCollectionViewCellViewModel.swift
+++ RickAndMorty/ViewModels/CharacterDetail/RMCharacterPhotoCollectionViewCellViewModel.swift
@@ -12,4 +12,13 @@ final class RMCharacterPhotoCollectionViewCellViewModel {
   init(imageUrl: URL?) {
     self.imageUrl = imageUrl
   }
+  
+  public func fetchImage(completion: @escaping (Result<Data, Error>) -> Void) {
+    
+    guard let imageUrl = imageUrl
+    else {
+      return completion(.failure(URLError(.badURL)))
+    }
+    RMImageLoader.shared.downloadImage(imageUrl, completion: completion)
+  }
 }
--- RickAndMorty/Views/CharacterDetails/RMCharacterPhotoCollectionViewCell.swift
+++ RickAndMorty/Views/CharacterDetails/RMCharacterPhotoCollectionViewCell.swift
@@ -9,9 +9,19 @@ import UIKit
 
 final class RMCharacterPhotoCollectionViewCell: UICollectionViewCell {
   static let cellIdentifier = "RMCharacterPhotoCollectionViewCell"
+  private let imageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.contentMode = .scaleAspectFill
+    imageView.clipsToBounds = true
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    return imageView
+  }()
+  
   
   override init(frame: CGRect) {
     super.init(frame: frame)
+    contentView.addSubview(imageView)
+    setUpConstaints()
   }
   
   required init?(coder: NSCoder) {
@@ -19,13 +29,28 @@ final class RMCharacterPhotoCollectionViewCell: UICollectionViewCell {
   }
   
   private func setUpConstaints() {
-    
+    NSLayoutConstraint.activate([
+      imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+      imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+      imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+      imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+    ])
   }
-
+  
   override func prepareForReuse() {
     super.prepareForReuse()
+    imageView.image = nil
   }
   public func configure(with viewModel: RMCharacterPhotoCollectionViewCellViewModel) {
-    
+    viewModel.fetchImage { [weak self] result in
+      switch result {
+      case .success(let data):
+        DispatchQueue.main.async {
+          self?.imageView.image = UIImage(data: data)
+        }
+      case .failure:
+        break
+      }
+    }
   }
 }
