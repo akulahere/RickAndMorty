d02426492c8208dedb16dd4242444485f2e7e2a7
Dmytro Akulinin <akulinind@gmail.com>
2023-03-03 11:43:23 +0200

    feat: add rate app feature

--- RickAndMorty/Controller/Core/RMSettingsViewController.swift
+++ RickAndMorty/Controller/Core/RMSettingsViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import SwiftUI
 import SafariServices
+import StoreKit
 
 /// Controller to show  various app options and settings
 final class RMSettingsViewController: UIViewController {
@@ -61,7 +62,10 @@ final class RMSettingsViewController: UIViewController {
       let vc = SFSafariViewController(url: url)
       present(vc, animated: true)
     } else if option == .rateApp {
-      
+        if let windowScene = view.window?.windowScene {
+          SKStoreReviewController.requestReview(in: windowScene)
+        
+      }
     }
   }
 }
